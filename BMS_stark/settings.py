b1288a9952b0ea51a8822ae0f63f391709ea697b
Venicid <liangshuo1994@outlook.com>
2018-09-01 22:32:51 +0800

    finish easy easy

--- BMS_stark/settings.py
+++ BMS_stark/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BMS_stark.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'stark/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -122,5 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'stark/static')
 ]
